6500e31fa68b5144db37a2531e1061fd1e726ff2
Vlad <vladyslav.hryn.25@kse.org.ua>
2026-04-07 18:41:43 +0300

    Character

--- Week10Character/Character.cs
+++ Week10Character/Character.cs
@@ -1,6 +1,20 @@
 namespace Week10Character;
 
-public class Character
-{
-    
+public enum CharacterStatus {Active, Wounded, Dead}
+public enum EventType {Rest, Combat, Healing, Tressure}
+public class Character 
+{ public string Name { get; set;} 
+    public string Role { get; set;}
+    public int Level { get; set; } 
+    public int HP { get; set; } 
+    public int Gold { get; set; }
+    public Character(string name, string role, int level, int hp, int gold, CharacterStatus status = CharacterStatus.Active) 
+    {
+        Name = name;
+        Role = role; 
+        Level = level; 
+        HP = hp;
+        Gold = gold;
+        
+    } 
 }
